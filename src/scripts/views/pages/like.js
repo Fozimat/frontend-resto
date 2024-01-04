c1d3f31215b2567df89436df8a107cd102754db3
fozimat <fozimata@gmail.com>
2024-01-04 22:20:57 +0700

    add condition if no favorite resto

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -9,7 +9,7 @@ const Like = {
             <div class="latest">
                 <h1 class="latest__label" tabindex="0">Favorite Restoran</h1>
                 <div class="posts" id="posts">
-
+                <p id="no_favorite" style="display:none;text-align:center;">Kamu belum memilih restoran favorit.</p>
                 </div>
             </div>
         </section>
@@ -20,9 +20,16 @@ const Like = {
   async afterRender() {
     const restaurants = await FavoriteRestoIdb.getAllRestaurants();
     const restaurantsContainer = document.querySelector("#posts");
-    restaurants.forEach((restaurant) => {
-      restaurantsContainer.innerHTML += createRestaurantsTemplate(restaurant);
-    });
+    const noFavoriteMessage = document.querySelector("#no_favorite");
+
+    if (restaurants.length === 0) {
+      noFavoriteMessage.style.display = "block";
+    } else {
+      restaurants.forEach((restaurant) => {
+        restaurantsContainer.innerHTML += createRestaurantsTemplate(restaurant);
+      });
+      noFavoriteMessage.style.display = "none";
+    }
   },
 };
 
