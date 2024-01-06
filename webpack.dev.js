5c77d241a46c53a3d48ea3e8dfd4b527a20b90fe
fozimat <fozimata@gmail.com>
2024-01-06 22:33:39 +0700

    bundle splitting

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
@@ -14,6 +16,34 @@ module.exports = merge(common, {
       overlay: {
         errors: true,
         warnings: true,
+      },
+    },
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerPort: 4000,
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: "~",
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
       },
     },
   },
